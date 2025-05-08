f7e642eb6be8bc29547dda8d00489cf21e803093
ranandasatria <rananda24@gmail.com>
2025-05-08 16:41:34 +0700

    feat: add input and button form to calculate celcius degree

---
+++ script.js
@@ -0,0 +1,35 @@
+const celcius = (document.getElementById('celcius').textContent);
+const fahrenheitResult = document.getElementById('fahrenheitResult');
+const reamurResult = document.getElementById('reamurResult');
+const kelvinResult = document.getElementById('kelvinResult');
+
+
+const form = document.getElementById('form-suhu')
+form.addEventListener('submit', function (event){
+    event.preventDefault()
+
+    const celcius = document.getElementById('celcius').value
+    if (isNaN(celcius)){
+        alert("Input harus berupa angka")
+        return;
+    }
+
+    fahrenheitResult.textContent = celciusToFahrenheit(celcius)
+    reamurResult.textContent = celciusToReamur(celcius)
+    kelvinResult.textContent = celciusToKelvin(celcius)
+})
+      
+
+function celciusToFahrenheit(celcius) {
+    return (celcius * 9 / 5) + 32;
+}
+
+
+function celciusToReamur(celcius) {
+    return (4 / 5 * celcius);
+}
+
+
+function celciusToKelvin(celcius) {
+    return celcius + 273;
+}
